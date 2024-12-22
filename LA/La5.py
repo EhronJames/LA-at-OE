d9962f8409a8adb2e211529ff7d9f88a4db98730
EhronJames <aeronlagamayo10@gmail.com>
2024-12-22 18:58:43 +0800

    Add files via upload

---
+++ LA/La5.py
@@ -0,0 +1,8 @@
+print("LA:5")
+class Character:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+    def __str__(self):
+        return f"{self.name} is a {self.role} hero."
