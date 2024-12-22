e2d71eb4eb18b0577913520d5e08f5e36042c991
EhronJames <aeronlagamayo10@gmail.com>
2024-12-22 19:02:33 +0800

    Add files via upload

---
+++ LA/La6.py
@@ -0,0 +1,10 @@
+print("LA:6")
+class Laptop:
+    def __init__(self, brand, model):
+        pass
+
+    def laptop_info(self):
+        pass
+laptop = Laptop("Huawei", "Lenovo")
+
+laptop.laptop_info()
