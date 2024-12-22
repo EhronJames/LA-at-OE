ac40079b85353955da1f980786d6975af2b2141e
EhronJames <aeronlagamayo10@gmail.com>
2024-12-22 21:15:47 +0800

    Add files via upload

---
+++ LA/La26.py
@@ -0,0 +1,13 @@
+print("LA:26")
+import turtles as c
+
+
+leo = c.Leonardo()
+mik = c.Michelangelo()
+don = c.Donatello()
+rap = c.Raphael()
+
+print(leo.name)
+print(mik.name)
+print(don.name)
+print(rap.name)
